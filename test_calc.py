a125e51f9f76dda8f0f09bb9b890d83a1b6cc4ad
Smeths <johnsmethurst@gmail.com>
2017-12-27 14:55:15 +0000

    testing travis again

--- test_calc.py
+++ test_calc.py
@@ -4,7 +4,7 @@ import calc
 class TestCalc(unittest.TestCase):
     
     def test_add(self):
-        self.assertEqual(calc.add(10,5),16)
+        self.assertEqual(calc.add(10,5),15)
         self.assertEqual(calc.add(-2,2),0)
         self.assertEqual(calc.add(-2,-3),-5)
 
